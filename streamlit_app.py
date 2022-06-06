754c739c33fefa546c47d4736870da957dac55ac
Aiad <106074630+AiadN@users.noreply.github.com>
2022-06-06 13:28:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #lets put  apick list here so they can pick the fruit
-streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+Fruits_selected = streamlit.multiselect("Pick some fruit:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+Fruits_to_show = my_fruit_list.loc[Fruits_selected]
 
 #display the table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(Fruits_to_show)
 
 
